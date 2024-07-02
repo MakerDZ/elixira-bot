0e111b943f678640913230811a9d8844e1f34f49
MakerDZ <mcskb91@gmail.com>
2024-07-03 00:24:30 +0400

    able to listen messages

--- config/runtime.exs
+++ config/runtime.exs
@@ -3,4 +3,9 @@ import Dotenvy
 
 source!([".env", System.get_env()])
 config :nostrum,
-  token: env!("BOT_TOKEN", :string!)
+  token: env!("BOT_TOKEN", :string!),
+  gateway_intents: [
+    :guilds,
+    :guild_messages,
+    :message_content
+  ]
